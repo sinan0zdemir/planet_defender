a45234a5daef3662703042424bee8bd392a55d75
ElisSudeAlkan <167646383+ElisSudeAlkan@users.noreply.github.com>
2024-06-02 16:45:21 +0300

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,12 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class EnemySpawner : MonoBehaviour
 {
-
-    [Header("Referances")]
+    [Header("References")]
     [SerializeField] private GameObject[] enemyPrefabs;
 
     [Header("Attributes")]
@@ -14,67 +12,86 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float enemiesPerSecond = 0.5f;
     [SerializeField] private float waveDelay = 5f;
     [SerializeField] private float difficultyScale = 0.75f;
-    
+
     [Header("Events")]
     public static UnityEvent onEnemyKilled = new UnityEvent();
-    
-    
+
     private int currentWave = 1;
     private float timeSinceLastSpawn;
     private int enemiesAlive;
     private int enemiesLeftToSpawn;
     private bool isSpawning = false;
 
+    private GameController gameController;
+
+    // Singleton instance
+    public static EnemySpawner instance;
+
     void Awake()
     {
-        onEnemyKilled.AddListener(EnemyKilled);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
 
+        onEnemyKilled.AddListener(EnemyKilled);
     }
 
-    // Start is called before the first frame update
     void Start()
     {
+        gameController = FindObjectOfType<GameController>(); // Find the GameController instance in the scene
         StartCoroutine(StartWave());
-
     }
 
-    // Update is called once per frame
-    void Update(){
-        if(!isSpawning) return;
+    void Update()
+    {
+        if (!isSpawning) return;
         timeSinceLastSpawn += Time.deltaTime;
 
-        if (timeSinceLastSpawn >= (1f/enemiesPerSecond) && enemiesLeftToSpawn > 0){
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
+        {
             SpawnEnemy();
             enemiesLeftToSpawn--;
             enemiesAlive++;
             timeSinceLastSpawn = 0f;
         }
 
-        if(enemiesAlive == 0 && enemiesLeftToSpawn == 0){
+        if (enemiesAlive == 0 && enemiesLeftToSpawn == 0)
+        {
             EndWave();
         }
     }
 
-    private int EnemiesPerWave(){
+    private int EnemiesPerWave()
+    {
         return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScale));
     }
 
-    private IEnumerator StartWave(){
-        yield return new  WaitForSeconds(waveDelay);
+    private IEnumerator StartWave()
+    {
+        yield return new WaitForSeconds(waveDelay);
         isSpawning = true;
         enemiesLeftToSpawn = EnemiesPerWave();
     }
 
-    private void SpawnEnemy(){
-       GameObject enemyToSpawn = enemyPrefabs[0];
-       Instantiate(enemyToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+    private void SpawnEnemy()
+    {
+        GameObject enemyToSpawn = enemyPrefabs[0];
+        Instantiate(enemyToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
     }
 
-    private void EnemyKilled(){
+    public void EnemyKilled()
+    {
         enemiesAlive--;
+        gameController.AddScore(10); // Adding 10 points for each enemy killed
     }
 
-    private void EndWave(){
+    private void EndWave()
+    {
         isSpawning = false;
         timeSinceLastSpawn = 0f;
         currentWave++;
