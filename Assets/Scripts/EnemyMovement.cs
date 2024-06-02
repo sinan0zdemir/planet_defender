fd7405f8b2f0ac588ded95c44c22ebb656bdb346
ElisSudeAlkan <167646383+ElisSudeAlkan@users.noreply.github.com>
2024-06-02 16:46:21 +0300

    Update EnemyMovement.cs

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
-{   
+{
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
 
     [Header("Attributes ")]
-    [SerializeField] private float moveSpeed  = 2f;
+    [SerializeField] private float moveSpeed = 2f;
 
     private Transform target;
     private int pathIndex = 0;
@@ -22,22 +22,34 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(target.position, transform.position) <= 0.1f){
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
             pathIndex++;
 
-            if (pathIndex == LevelManager.main.path.Length){
-                EnemySpawner.onEnemyKilled.Invoke();
+            if (pathIndex == LevelManager.main.path.Length)
+            {
+                GameController.instance.EnemyPassed();
                 Destroy(gameObject);
                 return;
-            } else {
+            }
+            else
+            {
                 target = LevelManager.main.path[pathIndex];
             }
         }
     }
+
     void FixedUpdate()
     {
         Vector2 direction = (target.position - transform.position).normalized;
-
         rb.velocity = direction * moveSpeed;
+    }
+
+    private void OnDestroy()
+    {
+        if (pathIndex < LevelManager.main.path.Length)
+        {
+            EnemySpawner.instance.EnemyKilled();
+        }
     }
 }
